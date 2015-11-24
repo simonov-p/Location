132fbd5d6ead270d38f20aad49b294804d9cf45f
Petr Simonov <simonov-p@ya.ru>
2015-11-24 22:46:15 +0300

    Intent Service: transition

--- app/src/main/java/simonov/pk/myapplication/GeoFenceTransitionsIntentService.java
+++ app/src/main/java/simonov/pk/myapplication/GeoFenceTransitionsIntentService.java
@@ -4,7 +4,10 @@ import android.app.IntentService;
 import android.content.Intent;
 import android.util.Log;
 
+import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.GeofencingEvent;
+
+import java.util.List;
 
 /**
  * Created by petr on 24-Nov-15.
@@ -12,11 +15,7 @@ import com.google.android.gms.location.GeofencingEvent;
 public class GeoFenceTransitionsIntentService extends IntentService {
 
     protected static final String TAG = "gfservice";
-    /**
-     * Creates an IntentService.  Invoked by your subclass's constructor.
-     *
-     * @param name Used to name the worker thread, important only for debugging.
-     */
+
     public GeoFenceTransitionsIntentService() {
         super(TAG);
     }
@@ -29,6 +28,18 @@ public class GeoFenceTransitionsIntentService extends IntentService {
                     geofencingEvent.getErrorCode());
             Log.e(TAG, errorMessage);
             return;
+        }
+
+        // Get the transition type.
+        int geofenceTransition = geofencingEvent.getGeofenceTransition();
+
+        // Test that the reported transition was of interest.
+        if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER ||
+                geofenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT) {
+
+            // Get the geofences that were triggered. A single event can trigger multiple geofences.
+            List<Geofence> triggeringGeofences = geofencingEvent.getTriggeringGeofences();
+
         }
     }
 }
