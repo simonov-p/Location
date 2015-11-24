3b8aecb7f12b196ad99541c9bf353b3196f9bd90
Petr Simonov <simonov-p@ya.ru>
2015-11-24 22:42:37 +0300

    Intent Service: handle event error

--- app/src/main/java/simonov/pk/myapplication/GeoFenceTransitionsIntentService.java
+++ app/src/main/java/simonov/pk/myapplication/GeoFenceTransitionsIntentService.java
@@ -2,6 +2,7 @@ package simonov.pk.myapplication;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.util.Log;
 
 import com.google.android.gms.location.GeofencingEvent;
 
@@ -23,5 +24,11 @@ public class GeoFenceTransitionsIntentService extends IntentService {
     @Override
     protected void onHandleIntent(Intent intent) {
         GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
+        if (geofencingEvent.hasError()) {
+            String errorMessage = GeofenceErrorMessages.getErrorString(this,
+                    geofencingEvent.getErrorCode());
+            Log.e(TAG, errorMessage);
+            return;
+        }
     }
 }
