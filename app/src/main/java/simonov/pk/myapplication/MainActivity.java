f8f67b9508dfe4cf9cd628724b8d5a012bc8f963
Petr Simonov <simonov-p@ya.ru>
2015-11-24 22:14:46 +0300

    Implement buttons

--- app/src/main/java/simonov/pk/myapplication/MainActivity.java
+++ app/src/main/java/simonov/pk/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package simonov.pk.myapplication;
 
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -12,11 +13,16 @@ import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.ActivityRecognition;
 import com.google.android.gms.location.DetectedActivity;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
@@ -27,7 +33,8 @@ import java.util.Calendar;
 
 public class MainActivity  extends ActionBarActivity implements GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener,
-        LocationListener {
+        LocationListener,
+        ResultCallback<Status> {
 
     private final String LOG_TAG = "Location Services Lesson 2-1";
     private TextView locationView;
@@ -164,6 +171,16 @@ public class MainActivity  extends ActionBarActivity implements GoogleApiClient.
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onResult(Status status) {
+        if (status.isSuccess()) {
+            Log.e(LOG_TAG, "Successfully added activity detection.");
+
+        } else {
+            Log.e(LOG_TAG, "Error adding or removing activity detection: " + status.getStatusMessage());
+        }
+    }
+
     public class ActivityDetectionBroadcastReceiver extends BroadcastReceiver {
         protected static final String TAG = "receiver";
 
@@ -206,4 +223,38 @@ public class MainActivity  extends ActionBarActivity implements GoogleApiClient.
         }
     }
 
+    private PendingIntent getActivityDetectionPendingIntent() {
+        Intent intent = new Intent(this, DetectedActivitiesIntentService.class);
+
+        // We use FLAG_UPDATE_CURRENT so that we get the same pending intent back when calling
+        // requestActivityUpdates() and removeActivityUpdates().
+        return PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+    public void requestActivityUpdatesButtonHandler(View view) {
+        if (!mGoogleApiClient.isConnected()) {
+            Toast.makeText(this,getString(R.string.not_connected),Toast.LENGTH_SHORT).show();
+            return;
+        }
+        ActivityRecognition.ActivityRecognitionApi.requestActivityUpdates(
+                mGoogleApiClient,
+                Constants.DETECTION_INTERVAL_IN_MILLISECONDS,
+                getActivityDetectionPendingIntent()
+        ).setResultCallback(this);
+        requestUpdatesButton.setEnabled(false);
+        removeUpdatesButton.setEnabled(true);
+    }
+
+    public void removeActivityUpdatesButtonHandler(View view) {
+        if (!mGoogleApiClient.isConnected()) {
+            Toast.makeText(this, getString(R.string.not_connected), Toast.LENGTH_SHORT).show();
+            return;
+        }
+        // Remove all activity updates for the PendingIntent that was used to request activity
+        // updates.
+        ActivityRecognition.ActivityRecognitionApi.removeActivityUpdates(
+                mGoogleApiClient,
+                getActivityDetectionPendingIntent()
+        ).setResultCallback(this);
+    }
 }
